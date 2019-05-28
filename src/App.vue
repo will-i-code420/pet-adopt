21dda85b611d675c0f2bf2af18a4f06641638791
Will Aronow <john.aronow@gmail.com>
2019-05-28 15:24:45 -0700

    added | between page nav links

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/cats">Cats</router-link>
-      <router-link to="/dogs">Dogs</router-link>
+      <router-link to="/cats">Cats</router-link> |
+      <router-link to="/dogs">Dogs</router-link> |
       <router-link to="/pet">Pets</router-link>
     </div>
     <router-view/>
