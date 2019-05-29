6f673058ca84bafaae090843eec655e2cd51bb2c
Will Aronow <john.aronow@gmail.com>
2019-05-28 17:05:58 -0700

    added files with fake data

---
+++ src/data/cats.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    name: 'Zeus',
+    breed: 'calico',
+    gender: 'female',
+    age: 4,
+    color: 'red/orange',
+    weight: 18,
+    location: 'Cage #3',
+    notes: 'likes her head rubbed'
+  },
+  {
+    name: 'Noodles',
+    breed: 'unknown',
+    gender: 'male',
+    age: 6,
+    color: 'black/white',
+    weight: 25,
+    location: 'Cage #8',
+    notes: 'tries to escape when caged opened'
+  },
+  {
+    name: 'Garfield',
+    breed: 'tabby',
+    gender: 'male',
+    age: 8,
+    color: 'orange w/ black spots',
+    weight: 33,
+    location: 'Cage #12',
+    notes: 'likes lasagna'
+  },
+  {
+    name: 'Chedder',
+    breed: 'tabby',
+    gender: 'female',
+    age: 2,
+    color: 'white',
+    weight: 16,
+    location: 'Cage #6',
+    notes: 'needs pain meds every 6 hours'
+  },
+  {
+    name: 'Tina',
+    breed: 'egyption',
+    gender: 'female',
+    age: 1,
+    color: 'white, no fur',
+    weight: 12,
+    location: 'Cage #3',
+    notes: 'allergic to grass'
+  },
+  {
+    name: 'Chester',
+    breed: 'unknown',
+    gender: 'male',
+    age: 9,
+    color: 'black',
+    weight: 30,
+    location: 'Cage #2',
+    notes: 'give him a treat before letting out'
+  }
+]
---
+++ src/data/dogs.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    name: 'Elwood',
+    breed: 'airdale',
+    gender: 'male',
+    age: 5,
+    color: 'brown/black',
+    weight: 104,
+    location: 'Cage #15',
+    notes: 'does not get along with other dogs'
+  },
+  {
+    name: 'Bebe',
+    breed: 'boston terrier',
+    gender: 'female',
+    age: 15,
+    color: 'black/white',
+    weight: 15,
+    location: 'Cage #20',
+    notes: 'very friendly, likes to be with other dogs'
+  },
+  {
+    name: 'Lolita',
+    breed: 'pitbull',
+    gender: 'female',
+    age: 4,
+    color: 'light gold',
+    weight: 65,
+    location: 'Cage #21',
+    notes: 'friends with Bebe'
+  },
+  {
+    name: 'Titan',
+    breed: 'great dane',
+    gender: 'male',
+    age: 7,
+    color: 'gray w/ black spots',
+    weight: 198,
+    location: 'Cage #29',
+    notes: 'likes to bite onto your arm and drag you around'
+  },
+  {
+    name: 'Gidget',
+    breed: 'boston terrier',
+    gender: 'female',
+    age: .6,
+    color: 'white w/red splashes',
+    weight: 18,
+    location: 'Cage #25',
+    notes: 'needs vitamin supplements every 12 hours'
+  },
+  {
+    name: 'Luna',
+    breed: 'golden lab',
+    gender: 'female',
+    age: 1,
+    color: 'gold',
+    weight: 57,
+    location: 'Cage #19',
+    notes: 'gets along with cats'
+  }
+]
