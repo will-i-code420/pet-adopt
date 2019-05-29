dbdbadd98a7e5543052be23c5e3d189535368e15
Will Aronow <john.aronow@gmail.com>
2019-05-29 12:35:28 -0700

    added add pet function to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,78 @@
 <template>
   <div class="home">
     <h1>Find Your New Best Friend Today!</h1>
+    <button @click="togglePetForm" class="btn btn-primary">Add New Pet</button>
+    <b-form @submit.prevent="submitPet" v-if="showPetForm">
+      <b-form-group id="input-group-2" label="Pet's Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="formData.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Species:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="formData.species"
+          :options="['cats', 'dogs']"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" label="Pet's Age:" label-for="input-4">
+        <b-form-select
+          id="input-4"
+          v-model="formData.age"
+          required
+          placeholder="Enter Age"
+        ></b-form-select>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { mapActions } from 'vuex'
 export default {
   name: 'home',
-  components: {
-
+  data () {
+    return {
+      showPetForm: false,
+      formData: {
+        name: '',
+        age: 0,
+        species: null
+      }
+    }
+  },
+  methods: {
+    ...mapActions ([
+      'addPet'
+    ]),
+    togglePetForm () {
+      this.showPetForm = !this.showPetForm
+    },
+    submitPet () {
+      const { species, age, name } = this.formData
+      const payload = {
+        species,
+        pet: {
+          name,
+          age
+        }
+      }
+      this.addPet(payload)
+      this.formData = {
+        name: '',
+        age: 0,
+        species: null
+      }
+    }
   }
 }
 </script>
