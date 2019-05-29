927c41bd9f825951e7f2cadddb409dae8499b4a8
Will Aronow <john.aronow@gmail.com>
2019-05-28 17:42:42 -0700

    updated App.vue and router to get ready to have Pet.vue open specifically clicked dog or cat off its own page

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/cats">Cats</router-link> |
-      <router-link to="/dogs">Dogs</router-link> |
-      <router-link to="/pet">Pets</router-link>
+      <router-link to="/dogs">Dogs</router-link>
     </div>
     <b-container>
       <router-view/>
--- src/router.js
+++ src/router.js
@@ -27,7 +27,7 @@ export default new Router({
       component: Dogs
     },
     {
-      path: '/pet',
+      path: '/pets/:id',
       name: 'pet',
       component: Pet
     }
