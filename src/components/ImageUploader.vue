56d0430c28917d29743b41f59fc246c67cdef37f
Will Aronow <john.aronow@gmail.com>
2019-05-31 17:10:40 -0700

    added js to ImageUploader.vue componenet

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -3,7 +3,7 @@
     <div @click="launchFilePicker()">
       <slot name="imageActivator"></slot>
       <input type="file" ref="file"
-      :name="uploadField"
+      :name="uploadFieldName"
       @change="onFileChange($event.target.name, $event.target.files)"
       style="display:none"
       >
@@ -26,6 +26,40 @@
 
 <script>
 export default {
+  name: 'image-input',
+  data: ()=> ({
+    errorDialog: null,
+    errorText: '',
+    uploadFieldName: 'file',
+    maxSize: 1024
+  }),
+  props: {
+    value: Object
+  },
+  methods: {
+    launchFilePicker () {
+      this.$refs.file.click()
+    },
+    onFileChange (fieldName, file) {
+      const { maxSize } = this
+      let imageFile = file[0]
+      if (file.length > 0) {
+        let size = imageFile.size
+      }
+      if (!imageFile.type.match('image.*')) {
+        this.errorDialog = true
+        this.errorText = 'Please choose an image file'
+      } else if (size > 1) {
+        this.errorDialog = true
+        this.errorText = 'Your file is too big! Please select an image under 1MB'
+      } else {
+        let formData = new FormData()
+        let imageURL = URL.createObjectURL(imageFile)
+        formData.append(fieldName, imageFile)
+      }
+      this.$emit('input', { formData, imageURL })
+    }
+  },
 }
 </script>
 
